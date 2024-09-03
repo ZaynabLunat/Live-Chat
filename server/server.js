68c752349c945e7b531edc2b5c4429fcfbf81487
ZaynabLunat <lunat815@gmail.com>
2024-09-03 14:44:23 +0100

    first commit

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import { WebSocketServer } from "ws";
+import WebSocket from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+// starts server instance on http://localhost:8080
+
+// waits for connection to be established from the client
+// the callback argument ws is a unique for each client
+wss.on("connection", (ws) => {
+  // runs a callback on message event
+  ws.on("message", (data) => {
+    // sends the data to all connected clients
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
+});
