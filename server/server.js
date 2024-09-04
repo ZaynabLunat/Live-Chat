794e65c7851bb7cd2b674af26dbd889b47a02df6
Lawrence Cilia <cilialawrence@yahoo.co.uk>
2024-09-04 15:20:31 +0100

    add live users counter

--- server/server.js
+++ server/server.js
@@ -8,6 +8,9 @@ const wss = new WebSocketServer({ port: 8080 });
 // waits for connection to be established from the client
 // the callback argument ws is a unique for each client
 wss.on("connection", (ws) => {
+
+  wss.clients.forEach(client => client.send(wss.clients.size));
+  
   // runs a callback on message event
   ws.on("message", (data) => {
     // sends the data to all connected clients
